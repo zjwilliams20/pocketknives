e93e37b2845f68a756fd9b0c676ef2ae79f99e47
Zach Williams <zjwilliams20@gmail.com>
2022-06-08 22:57:17 -0500

    Added matplotlib boundary setter with zoom capability

--- pocketknives/python/__init__.py
+++ pocketknives/python/__init__.py
@@ -1,4 +1,4 @@
-from .graphics import ticks_off
+from .graphics import ticks_off, set_bounds
 from .logger import ColorFormatter, get_logger
 from .numerical import factors, approx_lte, approx_gte, ascolumns, asrows
 from .tools import dump_locals, uncomment_json, timer, brief, eng_string
